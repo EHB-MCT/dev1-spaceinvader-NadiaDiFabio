112eb3b2b8cc6d40e316dd0d01a373bddfe53e66
Nadioune <nadia.di.fabio@student.ehb.be>
2022-10-11 16:59:50 +0100

    Taak Spaceinvader gedaan

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,45 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawLine();
+
+function drawLine() {
+
+context.fillStyle = "black";
+context.beginPath();
+context.rect(50, 50, 300, 300);
+context.fill();
+
+context.fillStyle = "#00ff00";
+context.beginPath();
+context.rect(75, 75, 250, 50);
+context.fill();
+
+context.fillStyle = "#00ff00";
+context.beginPath();
+context.rect(125, 175, 150, 50);
+context.fill();
+
+context.fillStyle = "#00ff00";
+context.beginPath();
+context.rect(75, 225, 50, 50);
+context.fill();
+
+context.fillStyle = "#00ff00";
+context.beginPath();
+context.rect(275, 225, 50, 50);
+context.fill();
+
+context.fillStyle = "#00ff00";
+context.beginPath();
+context.rect(175, 275, 50, 50);
+context.fill();
+
+}
+
+
+
